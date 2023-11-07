1ef7b9bf4437783d64368efe6ba8f9bfa38edd05
Siqi Liu <siqiliu@Siqis-MBP.attlocal.net>
2023-11-06 21:10:16 -0800

    add another way for resolving leetnode problem 236

--- lc236-lcaofbt/dfs/dfs.go
+++ lc236-lcaofbt/dfs/dfs.go
@@ -1,6 +1,8 @@
 package dfs
 
-import "dfs_leetcode/utils"
+import (
+	"dfs_leetcode/utils"
+)
 
 /*
 case 1: if root != p != q and p found on left and q found or right, return root
@@ -13,15 +15,15 @@ func LowestCommonAncestor(root, p, q *utils.TreeNode) *utils.TreeNode {
 		return nil
 	}
 
-	if found(root.Left, p) && found(root.Right, q) {
+	if (found(root.Left, p) && found(root.Right, q)) || (found(root.Left, q) && found(root.Right, p)) {
 		return root
 	}
 
-	if root.Val == p.Val && found(root.Right, q) {
+	if (root.Val == p.Val && found(root.Right, q)) || (root.Val == p.Val && found(root.Left, q)) {
 		return root
 	}
 
-	if found(root.Left, p) && root.Val == q.Val {
+	if (found(root.Left, p) && root.Val == q.Val) || (found(root.Right, p) && root.Val == q.Val) {
 		return root
 	}
 
@@ -48,4 +50,70 @@ func found(root, node *utils.TreeNode) bool {
 	}
 
 	return found(root.Left, node) || found(root.Right, node)
+}
+
+func nodeAddr(root, node *utils.TreeNode) *utils.TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == node.Val {
+		return root
+	}
+
+	res := nodeAddr(root.Left, node)
+	if res != nil {
+		return res
+	}
+
+	return nodeAddr(root.Right, node)
+}
+
+// map children to parents
+func LowestCommonAncestor2(root, p, q *utils.TreeNode) *utils.TreeNode {
+	childToParents := make(map[*utils.TreeNode]*utils.TreeNode)
+	fillMap(childToParents, root)
+	childToParents[root] = nil
+
+	pParents := make(map[*utils.TreeNode]int)
+	getParents(p, childToParents, pParents)
+
+	return findParent(q, childToParents, pParents)
+}
+
+func fillMap(childToParents map[*utils.TreeNode]*utils.TreeNode, root *utils.TreeNode) {
+	if root == nil {
+		return
+	}
+
+	if root.Left != nil {
+		childToParents[root.Left] = root
+	}
+
+	if root.Right != nil {
+		childToParents[root.Right] = root
+	}
+
+	fillMap(childToParents, root.Left)
+	fillMap(childToParents, root.Right)
+}
+
+func getParents(node *utils.TreeNode, childToParents map[*utils.TreeNode]*utils.TreeNode, parents map[*utils.TreeNode]int) {
+	if node == nil {
+		return
+	}
+
+	parents[node] = 0
+
+	nextNode := childToParents[node]
+
+	getParents(nextNode, childToParents, parents)
+}
+
+func findParent(node *utils.TreeNode, childToParents map[*utils.TreeNode]*utils.TreeNode, parents map[*utils.TreeNode]int) *utils.TreeNode {
+	if _, found := parents[node]; found {
+		return node
+	}
+
+	return findParent(childToParents[node], childToParents, parents)
 }
--- lc236-lcaofbt/dfs/dfs_test.go
+++ lc236-lcaofbt/dfs/dfs_test.go
@@ -85,3 +85,20 @@ func TestRightSideView4(t *testing.T) {
 		So(res.Val, ShouldResemble, 1)
 	})
 }
+
+func TestLowestCommonAncestor2(t *testing.T) {
+	Convey("1", t, func() {
+		input := "[3,5,1,6,2,0,8,null,null,7,4]"
+		root := utils.BuildTreeFromStringArray(input)
+		p := utils.TreeNode{
+			Val: 6,
+		}
+		pAddr := nodeAddr(root, &p)
+		q := utils.TreeNode{
+			Val: 8,
+		}
+		qAddr := nodeAddr(root, &q)
+		res := LowestCommonAncestor2(root, pAddr, qAddr)
+		So(res.Val, ShouldResemble, 3)
+	})
+}
