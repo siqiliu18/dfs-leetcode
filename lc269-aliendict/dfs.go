913570ea79fa863383d89bae884b882267ae6163
Siqi Liu <siqiliu@Siqis-MacBook-Pro.local>
2023-11-28 15:36:07 -0800

    how to avoid 2 parents 1 child duplicated case

--- lc269-aliendict/dfs.go
+++ lc269-aliendict/dfs.go
@@ -100,3 +100,92 @@ func AlienOrderBFS(words []string) string {
 
 	return res
 }
+
+func AlienOrderPostOrderDFS(words []string) string {
+
+	// similar to other languages we want to create a map of string to set here
+	// but since Go doesn't have set, we create a map of string to map of bool(doesn't matter) instead
+
+	// collect child nodes of an existing node based on giving input
+	nodeChildren := make(map[string]map[string]bool)
+
+	// collect parent nodes of an existing node based on giving input
+	nodeParents := make(map[string]map[string]bool)
+
+	// init child 'set' and parent 'set' of each node
+	for _, word := range words {
+		for _, char := range word {
+			nodeChildren[string(char)] = make(map[string]bool)
+			nodeParents[string(char)] = make(map[string]bool)
+		}
+	}
+
+	// loop through word list
+	for i := 0; i < len(words)-1; i++ {
+		w1 := words[i]
+		w2 := words[i+1]
+
+		// ex: word1 = abc, word2 = ab, this claim is not following the sorted lexicographically rule
+		if len(w1) > len(w2) && w1[:len(w2)] == w2 {
+			return ""
+		}
+
+		// comparing chars between two words
+		for j := 0; j < len(w1); j++ {
+			// if not the same, then w2 char is the child or w1 char
+			if w1[j] != w2[j] {
+				nodeChildren[string(w1[j])][string(w2[j])] = true
+				nodeParents[string(w2[j])][string(w1[j])] = true
+				break
+			}
+		}
+	}
+
+	res := ""
+
+	stack := []string{"0"}
+	nodeChildren["0"] = make(map[string]bool)
+
+	// extract nodes that have no parents
+	for node, cs := range nodeParents {
+		if len(cs) == 0 {
+			nodeChildren["0"][node] = true
+		}
+	}
+
+	if _, ok := nodeChildren["0"]; !ok {
+		return ""
+	}
+
+	// if queue isn't empty
+	for len(stack) != 0 {
+		node := stack[len(stack)-1] // pop the last node
+
+		if len(nodeChildren[node]) == 0 {
+			stack = stack[:len(stack)-1]
+		}
+
+		for ch := range nodeChildren[node] {
+			if len(nodeChildren[ch]) == 0 {
+				res += ch // missing the begging node
+				delete(nodeParents[ch], node)
+				delete(nodeChildren[node], ch) // most important
+				delete(nodeChildren, ch)
+				delete(nodeParents, ch)
+				// stack = stack[:len(stack)-1]
+			} else {
+				stack = append(stack, ch)
+			}
+		}
+	}
+
+	return Reverse(res)
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
--- lc269-aliendict/dfs_test.go
+++ lc269-aliendict/dfs_test.go
@@ -6,10 +6,58 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestExist1(t *testing.T) {
+func TestExistBFS1(t *testing.T) {
 	Convey("1", t, func() {
 		words := []string{"wrt", "wrf", "er", "ett", "rftt"}
 		res := AlienOrderBFS(words)
 		So(res, ShouldEqual, "wertf")
+	})
+}
+
+func TestExistBFS2(t *testing.T) {
+	Convey("2", t, func() {
+		words := []string{"zy", "zx"}
+		res := AlienOrderBFS(words)
+		So(res, ShouldEqual, "yxz")
+	})
+}
+
+func TestExistBFS3(t *testing.T) {
+	Convey("2", t, func() {
+		words := []string{"ac", "ab", "b"}
+		res := AlienOrderBFS(words)
+		So(res, ShouldEqual, "acb")
+	})
+}
+
+func TestExistDFS2(t *testing.T) {
+	Convey("2", t, func() {
+		words := []string{"wrt", "wrf", "er", "ett", "rftt"}
+		res := AlienOrderPostOrderDFS(words)
+		So(res, ShouldEqual, "wertf")
+	})
+}
+
+func TestExistDFS3(t *testing.T) {
+	Convey("3", t, func() {
+		words := []string{"z", "x", "z"}
+		res := AlienOrderPostOrderDFS(words)
+		So(res, ShouldEqual, "")
+	})
+}
+
+func TestExistDFS4(t *testing.T) {
+	Convey("4", t, func() {
+		words := []string{"zy", "zx"}
+		res := AlienOrderPostOrderDFS(words)
+		So(res, ShouldEqual, "yxz")
+	})
+}
+
+func TestExistDFS5(t *testing.T) {
+	Convey("5", t, func() {
+		words := []string{"ac", "ab", "b"}
+		res := AlienOrderPostOrderDFS(words)
+		So(res, ShouldEqual, "acb")
 	})
 }
