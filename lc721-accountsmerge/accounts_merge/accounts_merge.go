0360630e5c7e1cbbbfddb192fc0f4d91f53c46be
Siqi Liu <siqiliu@Siqis-MacBook-Pro.local>
2023-12-05 11:24:01 -0800

    finish problem 721 with slow solution

--- lc721-accountsmerge/accounts_merge/accounts_merge.go
+++ lc721-accountsmerge/accounts_merge/accounts_merge.go
@@ -1,6 +1,7 @@
 package accountsmerge
 
 import (
+	"fmt"
 	"sort"
 )
 
@@ -22,7 +23,7 @@ Expected:
 		["John","john00@mail.com","john_newyork@mail.com","johnnybravo@mail.com","johnsmith@mail.com"]
 	]
 */
-func AccountsMerge(accounts [][]string) [][]string {
+func AccountsMergeNotWorking(accounts [][]string) [][]string {
 	res := [][]string{}
 
 	acMap := make(map[string]map[string]byte)
@@ -93,4 +94,161 @@ func AccountsMerge(accounts [][]string) [][]string {
 		res = append(res, remainAcc)
 	}
 	return res
+}
+
+/*
+
+create a map of email string to int
+loop through the input table, every found email, added to map and increment its val by 1
+
+for emails with val > 1, meaning shared account found
+
+* This way not work for different 2 Johns
+
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+			{"John", "johnnybravo@mail.com", "johnnybravo1@mail.com"},
+		}
+----------
+
+create map[dup-email]obj where
+obj
+{
+	name
+	count
+	map[email]int
+}
+
+*/
+
+type AccountEmailCount struct {
+	Name    string
+	Visited bool
+	Emails  []string
+}
+
+func AccountsMergeBFS(accounts [][]string) [][]string {
+	res := [][]string{}
+
+	// create graph
+	eMap := make(map[string]*AccountEmailCount) // the val is a pointer instead of a copy, this way we can directly modify its porperties
+	for _, account := range accounts {
+		name := account[0]
+		emails := account[1:]
+		for i, email := range emails {
+			otherEmails := make([]string, len(emails)-1)
+			otherEmails = append(otherEmails, emails[:i]...)
+			otherEmails = append(otherEmails, emails[i+1:]...)
+			if entry, ok := eMap[email]; ok { // found
+				entry.Emails = append(entry.Emails, otherEmails...)
+				// if not *AccountEmailCount, we have to do ' eMap[email] = entry
+			} else { // first time
+				eMap[email] = &AccountEmailCount{
+					Name:   name,
+					Emails: otherEmails,
+				}
+			}
+		}
+	}
+
+	for k, v := range eMap {
+		fmt.Printf("email = %v, emails = %v", k, v.Emails)
+		fmt.Println()
+	}
+
+	// BFS begins
+	for email, property := range eMap {
+		if property.Visited {
+			continue
+		}
+
+		account := []string{}
+		queue := []string{email}
+
+		for len(queue) != 0 {
+			accEmail := queue[0]
+			queue = queue[1:]
+			account = append(account, accEmail)
+			eMap[accEmail].Visited = true
+
+			for _, childEmail := range eMap[accEmail].Emails {
+				if _, ok := eMap[childEmail]; ok && !eMap[childEmail].Visited {
+					queue = append(queue, childEmail)
+					eMap[childEmail].Visited = true // avoid appending the same email twice
+				}
+			}
+		}
+		sort.SliceStable(account, func(i, j int) bool {
+			return account[i] < account[j]
+		})
+		account = append([]string{property.Name}, account...)
+		res = append(res, account)
+	}
+
+	return res
+}
+
+func AccountsMergeDFS(accounts [][]string) [][]string {
+	res := [][]string{}
+
+	// create graph
+	eMap := make(map[string]*AccountEmailCount) // the val is a pointer instead of a copy, this way we can directly modify its porperties
+	for _, account := range accounts {
+		name := account[0]
+		emails := account[1:]
+		for i, email := range emails {
+			otherEmails := make([]string, len(emails)-1)
+			otherEmails = append(otherEmails, emails[:i]...)
+			otherEmails = append(otherEmails, emails[i+1:]...)
+			if entry, ok := eMap[email]; ok { // found
+				entry.Emails = append(entry.Emails, otherEmails...)
+				// if not *AccountEmailCount, we have to do ' eMap[email] = entry
+			} else { // first time
+				eMap[email] = &AccountEmailCount{
+					Name:   name,
+					Emails: otherEmails,
+				}
+			}
+		}
+	}
+
+	for k, v := range eMap {
+		fmt.Printf("email = %v, emails = %v", k, v.Emails)
+		fmt.Println()
+	}
+
+	// DFS begins
+	for email, property := range eMap {
+		if property.Visited {
+			continue
+		}
+
+		account := []string{}
+		property.Visited = true
+		dfs(&account, email, eMap)
+
+		sort.SliceStable(account, func(i, j int) bool {
+			return account[i] < account[j]
+		})
+		account = append([]string{property.Name}, account...)
+		res = append(res, account)
+	}
+
+	return res
+}
+
+func dfs(account *[]string, email string, eMap map[string]*AccountEmailCount) {
+	if eMap[email].Visited {
+		*account = append(*account, email)
+	}
+
+	for _, childEmail := range eMap[email].Emails {
+		if _, ok := eMap[childEmail]; ok && !eMap[childEmail].Visited {
+			eMap[childEmail].Visited = true
+			dfs(account, childEmail, eMap)
+		}
+	}
 }
--- lc721-accountsmerge/accounts_merge/accounts_merge_test.go
+++ lc721-accountsmerge/accounts_merge/accounts_merge_test.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestAccountsMerge(t *testing.T) {
+func TestAccountsMergeNotWorking(t *testing.T) {
 	Convey("1", t, func() {
 		accounts := [][]string{
 			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
@@ -14,7 +14,11 @@ func TestAccountsMerge(t *testing.T) {
 			{"Mary", "mary@mail.com"},
 			{"John", "johnnybravo@mail.com"}}
 		res := AccountsMergeNotWorking(accounts)
-		So(res, ShouldEqual, [][]string{{"John", "johnsmith@mail.com", "john_newyork@mail.com", "john00@mail.com"}, {"Mary", "mary@mail.com"}, {"John", "johnnybravo@mail.com"}})
+		So(res, ShouldEqual, [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+		})
 	})
 	Convey("2", t, func() {
 		accounts := [][]string{
@@ -25,6 +29,141 @@ func TestAccountsMerge(t *testing.T) {
 			{"John", "john00@mail.com", "johnnybravo@mail.com"},
 		}
 		res := AccountsMergeNotWorking(accounts)
-		So(res, ShouldEqual, [][]string{})
+		So(res, ShouldEqual, [][]string{
+			{"Mary", "mary@mail.com"},
+			{"John", "john00@mail.com", "john_newyork@mail.com", "johnnybravo@mail.com", "johnsmith@mail.com"},
+		})
+	})
+}
+
+func TestAccountsMergeBFS(t *testing.T) {
+	Convey("1", t, func() {
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"}}
+		res := AccountsMergeBFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+		})
+	})
+	Convey("2", t, func() {
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+			{"John", "johnnybravo@mail.com", "johnnybravo1@mail.com"},
+		}
+		res := AccountsMergeBFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com", "johnnybravo1@mail.com"},
+		})
+	})
+	Convey("3", t, func() {
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+			{"John", "john00@mail.com", "johnnybravo@mail.com"},
+			{"John", "john00@mail.com", "john01@mail.com"},
+		}
+		res := AccountsMergeBFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"Mary", "mary@mail.com"},
+			{"John", "john00@mail.com", "john_newyork@mail.com", "johnnybravo@mail.com", "johnsmith@mail.com"},
+		})
+	})
+}
+
+func TestAccountsMergeBFS2(t *testing.T) {
+	Convey("1", t, func() {
+		accounts := [][]string{
+			{"Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"},
+			{"Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"},
+			{"Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"},
+			{"Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"},
+			{"Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"},
+		}
+		res := AccountsMergeBFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"},
+			{"Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"},
+			{"Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"},
+			{"Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"},
+			{"Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"},
+		})
+	})
+}
+
+func TestAccountsMergeDFS1(t *testing.T) {
+	Convey("1", t, func() {
+		accounts := [][]string{
+			{"Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"},
+			{"Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"},
+			{"Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"},
+			{"Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"},
+			{"Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"},
+		}
+		res := AccountsMergeDFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"},
+			{"Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"},
+			{"Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"},
+			{"Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"},
+			{"Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"},
+		})
+	})
+}
+
+func TestAccountsMergeDFS2(t *testing.T) {
+	Convey("1", t, func() {
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"}}
+		res := AccountsMergeDFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+		})
+	})
+	Convey("2", t, func() {
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+			{"John", "johnnybravo@mail.com", "johnnybravo1@mail.com"},
+		}
+		res := AccountsMergeDFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com", "johnnybravo1@mail.com"},
+		})
+	})
+	Convey("3", t, func() {
+		accounts := [][]string{
+			{"John", "johnsmith@mail.com", "john_newyork@mail.com"},
+			{"John", "johnsmith@mail.com", "john00@mail.com"},
+			{"Mary", "mary@mail.com"},
+			{"John", "johnnybravo@mail.com"},
+			{"John", "john00@mail.com", "johnnybravo@mail.com"},
+			{"John", "john00@mail.com", "john01@mail.com"},
+		}
+		res := AccountsMergeDFS(accounts)
+		So(res, ShouldResemble, [][]string{
+			{"Mary", "mary@mail.com"},
+			{"John", "john00@mail.com", "john_newyork@mail.com", "johnnybravo@mail.com", "johnsmith@mail.com"},
+		})
 	})
 }
