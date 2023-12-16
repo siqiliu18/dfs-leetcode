d6042c344f002df096d92428a19073b08a4620a7
Siqi Liu <siqiliu@Siqis-MacBook-Pro.local>
2023-12-15 17:12:00 -0800

    don't understand

--- lc-17-46-permute/dfs/dfs.go
+++ lc-17-46-permute/dfs/dfs.go
@@ -66,6 +66,9 @@ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 Example 2:
 Input: nums = [0,1]
 Output: [[0,1],[1,0]]
+
+Explanation 1: https://www.youtube.com/watch?v=H232aocj7bQ (I think this one is better)
+Explanation 2: https://www.youtube.com/watch?v=Nabbpl7y4Lo&t=506s
 */
 func Permute(nums []int) [][]int {
 	res := new([][]int)
@@ -77,7 +80,15 @@ func Permute(nums []int) [][]int {
 
 func permRec(nums []int, combArr []int, visited []bool, res *[][]int) {
 	if len(combArr) == len(nums) {
-		*res = append(*res, combArr)
+		// *res = append(*res, combArr)
+		/*
+			it is interesting that by direct append *res = append(*res, combArr)
+			the res will be modified!!! See {5,4,6,2} example.
+			{{5,4,2,6},{5,4,2,6},...}
+		*/
+		// cp := make([]int, len(combArr))
+		// copy(cp, combArr) // deep copy
+		*res = append(*res, append([]int{}, combArr...)) // create new slice
 		return
 	}
 	for i := 0; i < len(nums); i++ {
@@ -89,5 +100,60 @@ func permRec(nums []int, combArr []int, visited []bool, res *[][]int) {
 		permRec(nums, combArr, visited, res)
 		combArr = combArr[:len(combArr)-1]
 		visited[i] = false
+	}
+}
+
+func permRec2(nums []int, combArr []int, res *[][]int) {
+	if len(combArr) == len(nums) {
+		*res = append(*res, append([]int{}, combArr...))
+		return
+	}
+	for _, num := range nums {
+		if contains(combArr, num) {
+			continue
+		}
+		combArr = append(combArr, num)
+		permRec2(nums, combArr, res)
+		combArr = combArr[:len(combArr)-1]
+	}
+}
+
+func contains(arr []int, num int) bool {
+	for _, v := range arr {
+		if v == num {
+			return true
+		}
+	}
+
+	return false
+}
+
+// why this one no need deep copy??????
+func Permute2(nums []int) [][]int {
+	o := [][]int{}
+	curHash := make(map[int]bool)
+	backtracking(nums, &o, []int{}, curHash)
+	return o
+}
+
+func backtracking(nums []int, o *[][]int, cur []int, curHash map[int]bool) {
+	if len(nums) == len(cur) {
+		*o = append(*o, cur)
+		return
+	}
+	// for each value in nums, start with nums[i]
+	for i := 0; i < len(nums); i++ {
+		// record existing values in hash, and skip if already used
+		if curHash[nums[i]] == true {
+			continue
+		}
+		curHash[nums[i]] = true
+		// add the next nums[i] value to current list
+		cur := append(cur, nums[i])
+		// recursively call function
+		backtracking(nums, o, cur, curHash)
+		// remove last item from cur
+		delete(curHash, cur[len(cur)-1])
+		cur = cur[:len(cur)-1]
 	}
 }
